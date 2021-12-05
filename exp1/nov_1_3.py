865ad9265e5f43554cd25413806f26fe2ee9321f
Nyanm <50958314+Nyanm@users.noreply.github.com>
2021-12-05 11:21:59 +0800

    Delete exp1 directory

--- exp1/nov_1_3.py
+++
@@ -1,9 +0,0 @@
-def get_solution(threshold: int) -> list:
-    res = []
-
-    for a in range(1, threshold + 1):
-        if not (a ** 2 - a) % 100:  # b is an integer
-            if 1 <= (a ** 2 - a) // 100 <= threshold:
-                res.append([a, (a ** 2 - a) // 100])
-
-    return res
